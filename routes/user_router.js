863726c9664bf83a35f354a5f166e94ff46a1f73
Toosdai Otte <toosdai52Otte@gmail.com>
2020-01-06 13:26:49 -0600

    nothing noteable

--- routes/user_router.js
+++ routes/user_router.js
@@ -4,7 +4,7 @@ const jsonWT = require('jsonwebtoken')
 
 const helpers = require('../helpers/user_helpers.js')
 
-router.get('/all', async (req, res) => {
+router.get('/', async (req, res) => {
 
     try {
         const users = await helpers.get()
@@ -15,6 +15,24 @@ router.get('/all', async (req, res) => {
     }
 })
 
+router.put('/', async (req, res) => {
+
+})
+
+router.delete('/', async (req, res) => {
+    
+})
+
 module.exports = router
 
 //testing
+// intergrate endpoint for user that is logged in
+// edit and delete user endpoints
+// create scores migration
+// GET endpoint for scores (global - all users, single users score)
+    // take query strings in url to sort all users scores (highest/lowest/etc)
+    // take query string in url to limit all users scores shown 
+
+// add date played to scores table
+
+// POST endpoint for scores
